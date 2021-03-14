7bbfc6725f80021c791e54e991ddd31f9a100550
Rano Kuhl <ranokuhl@gmail.com>
2021-03-14 17:01:54 +0100

    refactored ReactDOM

--- client/client.jsx
+++ client/client.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 
-ReactDom.render(<App />, document.querySelector("#Container"));
+ReactDOM.render(<App />, document.querySelector("#Container"));
